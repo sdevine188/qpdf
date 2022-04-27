4cb7df81502187a28a03e1506e80c149eaab073a
sdevine188 <sdevine188@gmail.com>
2022-04-27 14:57:01 -0400

    updated

--- qpdf_scratchpad.R
+++ qpdf_scratchpad.R
@@ -18,10 +18,34 @@ setwd("C:/Users/sdevine/Desktop")
 ?pdf_combine
 ?pdf_compress
 
-# combine pdf
+
+#///////////////////////////
+
+
+# manually assign doc_path variable
+
+# check files
 dir_ls()
-pdf_combine(input = c("aef - first page.pdf", "AEF - Devine last 8.pdf"),
-            output = "merged_pdf.pdf")
+
+# check length
+pdf_length(input = doc_path, password = "")
+
+# split will automatically save each of the split pages as pdfs in the output directory
+# pdf_subset(input = doc_path, pages = 1, output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_test_1.pdf"))
+# pdf_subset(input = doc_path, pages = 1:3, output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_test_2.pdf"))
+# pdf_subset(input = doc_path, pages = c(1, 3), output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_test_3.pdf"))
+# pdf_subset(input = doc_path, pages = -2, output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_test_4.pdf"))
+
+pdf_subset(input = "aef_old_version.pdf", pages = 1, output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_1.pdf"))
+pdf_subset(input = "aef_updated_unsigned.pdf", pages = 2:9, output = str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_2_to_9.pdf"))
 
 
+# combine pdf
+pdf_combine(input = c(str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_1.pdf"),
+                      str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_2_to_9.pdf")),
+            output = str_c(str_sub(string = doc_path, start = 1, end = -5), ".pdf"))
+
+# remove unnecessary files
+file_delete(path = c(str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_1.pdf"),
+                     str_c(str_sub(string = doc_path, start = 1, end = -5), "_pages_2_to_9.pdf")))
 
